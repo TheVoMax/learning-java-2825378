4957d42b329e6d3b657050bffaf2ec4ebd7d7a12
TheVoMax <77507991+TheVoMax@users.noreply.github.com>
2021-01-16 21:22:58 +0100

    AddingQuestions

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "Jaki mam kolor sciany?";
+        String choiceOne = "Zolty";
+        String choiceTwo = "Zielony";
+        String choiceThree = "Czerwony";
 
         String correctAnswer = choiceTwo;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
-
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
         // Have the user input an answer
+        Scanner scanner = new Scanner();
+        
+
         // Retrieve the user's input
 
         // If the user's input matches the correctAnswer...
