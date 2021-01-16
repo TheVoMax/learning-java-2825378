c9c2712e65d618fc2a5f6beccfda4378e9c2ac26
TheVoMax <77507991+TheVoMax@users.noreply.github.com>
2021-01-16 21:31:25 +0100

    AddedIf

--- src/Main.java
+++ src/Main.java
@@ -13,20 +13,25 @@ public class Main {
         // Write a print statement asking the question
         System.out.println(question);
         // Write a print statement giving the answer choices
-        System.out.println(choiceOne);
-        System.out.println(choiceTwo);
-        System.out.println(choiceThree);
+        System.out.println("a) " + choiceOne);
+        System.out.println("b) " + choiceTwo);
+        System.out.println("c) " + choiceThree);
         // Have the user input an answer
-        Scanner scanner = new Scanner();
-        
-
+        System.out.println("Odpowiedz: ");
         // Retrieve the user's input
+        Scanner input = new Scanner(System.in);
+        String answer = input.next();
 
         // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
+        if(answer.equals(correctAnswer)){
+            // then the user is correct and we want to print out a congrats message to the user.
+            System.out.println("Poprawna odpowiedz!");
+            // If the user's input does not match the correctAnswer...
+        } else {
+            // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+            System.out.println("Zle! Poprawna odpowiedz: " + correctAnswer);
+        }
 
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
 
     }
 
