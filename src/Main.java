3c61e0cc37eabaffd0ea61cec97408e54f7ea294
TheVoMax <77507991+TheVoMax@users.noreply.github.com>
2021-01-17 02:03:08 +0100

    Solution
    Chapter 5 challenge solution

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static double roczneWynagrodzenie(double godziny, double stawka, int wakacje){
+        int wak = wakacje * 8;
+        double godzWSumie = godziny * 52 - wak;
+        double zarobki = godzWSumie * stawka;
+        return zarobki;
+    }
+
     public static void main(String[] args) {
-        double result = Math.pow(2, 5);
-        System.out.println(result);
+        System.out.println("Podaj liczbe przepracowanych godzin w tygodniu: ");
+        Scanner input = new Scanner(System.in);
+        double przepracowaneGodziny = input.nextDouble();
+        System.out.println("Podaj stawke: ");
+        double stawkaGodz = input.nextDouble();
+        System.out.println("Podaj ilosc dni urlopowych: ");
+        int urlop = input.nextInt();
+        double roczneZarobki = roczneWynagrodzenie(przepracowaneGodziny, stawkaGodz, urlop);
+        System.out.println("Twoje roczne zarobki: " + roczneZarobki);
     }
 
 }
